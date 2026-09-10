0a1511db0c5bd5dfdb7fdb7623077f17ac39f89a
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-09-10 20:48:54 +0200

    Update bidon.spec.ts

--- Frontend/src/app/tests/bidon.spec.ts
+++ Frontend/src/app/tests/bidon.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 
 describe('test nul', () => {
 
   it('Test nul pour verifier', () => {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
   });
 });
