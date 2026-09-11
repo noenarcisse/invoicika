66909a0acba2c38814558d78b58916045a92d3ad
Noe <narcisse.noe@gmail.com>
2026-09-11 08:54:13 +0200

    puuush majed depuis BF

--- Frontend/src/app/pages/customer/customer-add/customer-add.component.spec.ts
+++ Frontend/src/app/pages/customer/customer-add/customer-add.component.spec.ts
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/angular';
-import { CustomerAddComponent } from './customer-add.component';
+// import { render, screen } from '@testing-library/angular';
+// import { CustomerAddComponent } from './customer-add.component';
 
-describe('CustomerAddComponent', () => {
-  it('should create', async () => {
-    await render(CustomerAddComponent)
-    // render(CustomerAddComponent)
-    // const { fixture } = await render(CustomerAddComponent);
-    // screen.findAllByRole()
-    // expect(fixture.componentInstance).toBeTruthy();
-  });
-});
+// describe('CustomerAddComponent', () => {
+//   it('should create', async () => {
+//     await render(CustomerAddComponent)
+//     // render(CustomerAddComponent)
+//     // const { fixture } = await render(CustomerAddComponent);
+//     // screen.findAllByRole()
+//     // expect(fixture.componentInstance).toBeTruthy();
+//   });
+// });
